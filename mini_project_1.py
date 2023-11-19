196a868921b8c01aefd776e5b64cd6809d65b703
thussain <tahseenreza101@gmail.com>
2023-11-18 19:43:08 -0800

    11/18, Tahseen Hussain
    Accomplished
    -----------
     - Comments explaining functions, paramters, and return values
     - Added functions to minimize code in main for one vs one &
    	one vs all classifiers
    
    In Progress
    -----------
     - Code cleanup
    
    Next Tasks
    -----------
     - Randomized features function
    
    Go up to line 72 because it cuts of any messages after line 72---------
    Describe what you changed, what functionality occured

--- mini_project_1.py
+++ mini_project_1.py
@@ -13,11 +13,29 @@ from collections import defaultdict
 mat = scipy.io.loadmat('mnist.mat')
 
 np.set_printoptions(threshold=100)  # Set threshold to infinity to print all elements
-def prediction_calc(arr):
+def prediction_calc(arr: np.array) -> np.array:
+    '''
+    Calculates predictions for OneVsOne Classifier
+    by tabulating the votes in each row. Ties 
+    are broken by picken the index of first apperance of 
+    equal tally.
     
+    
+    Parameters
+    ---------
+    arr: np.array
+        Holds votes of each, expected 10K x 45 
+        
+        
+        
+    Returns 
+    -------
+    predictions: np.array
+        Holds result of tabulated vote, ie classifier image prediction label
+
+    ''' 
     row,columns = np.shape(arr)
-    print(row)
-    print(columns)
+
     res = np.sum(arr == arr.max(axis=1, keepdims=True), axis=1) > 1
     prediction = np.zeros((10000,1))
     for y in range(row):
@@ -51,9 +69,21 @@ def visualize(img_array: np.array, index: int) -> None:
     
     
 def info(values, printr):
+    '''
+    Display Data - Debugger had high load time in some instances
     
-    # The debugger takes forever to load this is way quicker #dontjudgemepls
+    
+    Parameters
+    ---------
+    values: Any
+        Typically np.array, chosen item to display data of
+    printr: bool
+        to print or not print, that is the question
+    
+    Returns 
+    -------
 
+    ''' 
     print(type(values))
     print(np.shape(values))
     if printr == 1:
@@ -111,7 +141,7 @@ def sign(trainY: np.array, label_rqst: int) -> np.array:
     trainY_bin = np.where(trainY == label_rqst, 1, -1)
     
     return trainY_bin.T    
-def img_label_extractor(norm_nonzero_img: np.array, train_label: np.array, label_rqst1: int, label_rqst2:int, original_img) -> np.array:
+def img_label_extractor(norm_nonzero_img: np.array, train_label: np.array, label_rqst1: int, label_rqst2:int) -> np.array:
     '''
     Purpose: Extracts the img_data corresponding to the desired label 
     
@@ -121,23 +151,18 @@ def img_label_extractor(norm_nonzero_img: np.array, train_label: np.array, label
         Normalized and zeroed columns removed dataset 
     label_array: np.array
         True labels of our test image dataset 
-        
+    label_rqst1/2: ints
+        The requested image labels to extract from corresponding 
+        norm_nonzero_img array
         
     Returns 
     ----------
     labeled_img_data: np.array
         Extracted img data that is only of corresponding requested label  
         
-    General process
-    ----------
-    Check requested label 
-    - Get positions of labels in y matrix that matches with label_rqst
-    - Extract corresponding indices from normalized trainX set 
+
     ''' 
-    
-    # Checks for requested label across train_label array and stores into idx 
-    # Need to convert idx from np.array into flattened 1D list to correctly extract 
-    # corresponding image data 
+
     
 # Checks train_label indice values that match label_rqstN and stores the indice - slices indices from norm_nonzero_img
     #-------------------------------------
@@ -149,7 +174,9 @@ def img_label_extractor(norm_nonzero_img: np.array, train_label: np.array, label
     rqst_img_both = norm_nonzero_img[indx_net]
     #-------------------------------------
     
-# Finds elements that aren't labelrqst1 or 2 and removes them 
+# Finds elements that aren't labelrqst1 or 2 and removes them
+    #-------------------------------------
+ 
     idx1_r = np.argwhere(np.all(train_label[..., :] != label_rqst1,axis = 0 ))
     idx2_r = np.argwhere(np.all(train_label[..., :] != label_rqst2,axis = 0 ))
     idx1_rflat = list(chain.from_iterable(idx1_r.tolist()))
@@ -159,9 +186,11 @@ def img_label_extractor(norm_nonzero_img: np.array, train_label: np.array, label
     idx_r_common = [common for common in idx1_rflat if common in idx2_rflat]
 
     trainY_OVO = np.delete(train_label,idx_r_common)
+    #-------------------------------------
+
     return rqst_img_both, trainY_OVO
 
-def normal_eqn_solver(trainX_norm_nonzero_ones: np.array, trainY_bin: np.array) -> np.array:
+def normal_eqn_solver(trainX_norm_nonzero_ones: np.array, trainY: np.array, bin_class:int) -> np.array:
     '''
     Purpose: Computes the solution to the normal equation 
     
@@ -191,6 +220,7 @@ def normal_eqn_solver(trainX_norm_nonzero_ones: np.array, trainY_bin: np.array) 
     ## ADD IN SECTION ABOUT HOW THEY ARE NOT EQUAL!!!
 
 
+    trainY_bin = sign(trainY, bin_class)
 
     weights = np.dot(np.linalg.pinv(trainX_norm_nonzero_ones) , trainY_bin)
     return weights 
@@ -223,9 +253,37 @@ def binary_classifier(weights: np.array, X_N_test_ones: np.array) -> np.array:
     #isualize(norm_nonzero_img, 5000)
 
     #results = np.square(trainY-(np.dot(np.transpose(weights),norm_nonzero_img)))
+def one_v_all_weight_calcs(trainY: np.array, X_N_train_ones: np.array)->np.array:
+    '''
+    Calculates the weights for the one_v_all classifier
+    
+    
+    Parameters
+    ---------
+    trainY: np.array
+        Labels of training dataset
+    X_N_train_ones: np.array
+        Image data that is normalized, with ones column added
+        onto the end, and 0 columns removed
+        
+    Returns 
+    -------
+    weight_matrix: np.array
+        Solution of the normal equation, weights of each pixel 
+        information for ova classifier
+    ''' 
+    
+    
+    weight_list = []
+    for x in range(10):
+        weights = normal_eqn_solver(X_N_train_ones, trainY,x)
+        weight_list.append(weights)
+        print(x) # Indicates how many loops till completion 
+    weight_matrix = np.hstack(weight_list)
+    return weight_matrix
 def mc_one_vs_all_classifier(trainY: np.array, weights: np.array, X_N_test_ones: np.array):
     '''
-    Multiclass classifier model 
+    Multiclass classifier model built upon a one_vs_all weights 
     
     
     Parameters
@@ -251,13 +309,81 @@ def mc_one_vs_all_classifier(trainY: np.array, weights: np.array, X_N_test_ones:
     mc_ova_labels.append(np.argmax(confidence,axis=1))
 
     return mc_ova_labels
+def pair_calc():
+    '''
+    Calculates unique pairs from numbers 0-9
+    
+    
+    Parameters
+    ---------
+    N/A
+        
+        
+    Returns 
+    -------
+    45 unique pairs of numbers 0-9
+    ''' 
+    pairs = []
+    for i in range(10):
+        for j in range(i+1, 10):
+            pairs.append([i,j])
+    print(pairs[0])
+    print(len(pairs))
+    return pairs
+def one_vs_one_weight_calc(X_N_train_ones: np.array,trainY:np.array) -> np.array:
+    '''
+    Calculates the weights for the one vs one classifier
+    
+    
+    Parameters
+    ---------
+    trainY: np.array
+        Labels of training dataset
+    X_N_train_ones: np.array
+        Image data that is normalized, with ones column added
+        onto the end, and 0 columns removed
+    t
+    Returns 
+    -------
+    OVO_weight_matrix: np.array
+        Solution of the normal equation, weights of each pixel 
+        information for OVO classifier 
+    ''' 
+    
+    OVO_weight_list = []
+    pairs = pair_calc()
+    for x in range(len(pairs)):
+        v1,v2 = pairs[x][0],pairs[x][1] # replace with method that computes each possible combination 
+        trainX_OVO, trainY_OVO = img_label_extractor(X_N_train_ones, trainY,v1,v2)
+        OVO_weight = normal_eqn_solver(trainX_OVO,trainY_OVO, v1)
+        OVO_weight_list.append(OVO_weight)
+        print(x)
+    OVO_weight_matrix = np.array(OVO_weight_list, dtype='float32').T
 
+    return OVO_weight_matrix, pairs
 def mc_one_vs_one_classifier(labels: np.array,confidence,X_N_test_ones, pairs):
+    '''
+    Multiclass classifier model built upon one_vs_one weights 
+    
+    
+    Parameters
+    ---------
+    trainY: np.array 
+        Y label training dataset 
+    weights: array
+        weighted matrix of all weights of one vs all classifiers  
+        
+        
+    Returns 
+    -------
+    mc_ova_labels: np.array
+        prediction label array of ingested image dataset 
+    ''' 
+    
     print('one v one called')
     r, c = np.shape(labels)
     
-    tally = np.zeros((10000,10)) 
-    
+    tally = np.zeros((10000,10))
     row_vote = np.zeros((10000,1))
     
     for row in range(r): # replace with r when running full classifier 
@@ -301,8 +427,7 @@ def confusion_matrix(testY, classifier_labels) -> int:
         
 
     
-    info(testY,0)
-    info(classifier_labels,0)
+   
     error = 0 
 
     # Error Calculation 
@@ -359,17 +484,28 @@ def main():
     # --------------------------------------------------
 
 
-# Binary one versus all classifier test - 9 versus all 
+# Binary classifier - N versus all - N = [0,1,2,4,5,6,7,8,9]
     # -------------------------------------------------
     '''
-    binary_class = 9
-    trainY_bin = sign(trainY, binary_class)
+    
+    
+    bin_error = []
+    
+    for bin_class in range(10):
+    
+        # Calculates weights 
+        bin_weight = normal_eqn_solver(X_N_train_ones,trainY, bin_class)
+        
+        testY_bin = sign(testY,bin_class)
+        
+        # Classifies index as bin_class or not bin_class 
+        bin_classifier_predictions, confidence = binary_classifier(bin_weight,X_N_test_ones)   
 
-    bin_weight = normal_eqn_solver(X_N_train_ones,trainY_bin)
-    testY_bin = sign(testY,binary_class)
-    bin_classifier_predictions, confidence = binary_classifier(bin_weight,X_N_test_ones)   
-
-    confusion_matrix(testY_bin,bin_classifier_predictions)
+        # Stores error 
+        bin_error.append(confusion_matrix(testY_bin,bin_classifier_predictions))
+        
+    print(bin_error)
+    
     '''
     # --------------------------------------------------
 
@@ -378,16 +514,20 @@ def main():
     # --------------------------------------------------
     
     
+    
     '''
-    weight_list = []
-    for x in range(10):
-        trainY_bin = sign(trainY, x)
-        weights = normal_eqn_solver(X_N_train_ones, trainY_bin)
-        weight_list.append(weights)
-        print(x) # Indicates how many loops till completion 
-    weight_matrix = np.hstack(weight_list)
+    
+    
+    # Calculates weights
+    weight_matrix = one_v_all_weight_calcs(trainY, X_N_train_ones)
+    
+    # Creates predictions 
     mc_classifier_predictions = mc_one_vs_all_classifier(trainY, weight_matrix, X_N_test_ones)
-    MC_error = confusion_matrix(np.transpose(testY), np.transpose(mc_classifier_predictions))
+    
+    # Calculates Error 
+    one_v_all_error = confusion_matrix(np.transpose(testY), np.transpose(mc_classifier_predictions))
+    
+    print(one_v_all_error)
     '''
     # --------------------------------------------------
   
@@ -401,50 +541,30 @@ def main():
     '''
     # --------------------------------------------------
     print('Multi-class classifier - one versus one')
+     
     
-    # Data preparation 
-    pairs = []
-    print(testY[0][:10])
-    for i in range(10):
-        for j in range(i+1, 10):
-            pairs.append([i,j])
-    print(pairs[0])
-    print(len(pairs))
-    OVO_weight_list = []
-    #len(pairs)
-    for x in range(len(pairs)):
-        v1,v2 = pairs[x][0],pairs[x][1] # replace with method that computes each possible combination 
-        trainX_OVO, trainY_OVO = img_label_extractor(X_N_train_ones, trainY,v1,v2, trainX)
-        trainY_OVO_bin = sign(trainY_OVO,v1) # v1 will always be +1 
-        OVO_weight = normal_eqn_solver(trainX_OVO,trainY_OVO_bin)
-        OVO_weight_list.append(OVO_weight)
-        print(x)
-    OVO_weight_matrixhstack = np.hstack(OVO_weight_list)
-    OVO_weight_matrix = np.array(OVO_weight_list, dtype='float32').T
     
+    # Calculate weights & labels 
+    OVO_weight_matrix, pairs = one_vs_one_weight_calc(X_N_train_ones,trainY)
     labels, confidence = binary_classifier(OVO_weight_matrix,X_N_test_ones)
   
+  
+    # Produce predictions 
     pred_vec = mc_one_vs_one_classifier(labels,confidence, X_N_test_ones,pairs)
 
     print(f'testY  Val: {testY[0][:10]}')
     print(f'Prediction: {np.reshape(pred_vec, (1,10000))[0][:10]}')
     x = np.reshape(pred_vec, (1,10000))
-    confusion_matrix(np.transpose(testY), pred_vec)
     
+    # Calculate Error 
+    onevoneerror = confusion_matrix(np.transpose(testY), pred_vec)
 
+
+    print(f'One V one Error: {onevoneerror}')
+
+    
     # --------------------------------------------------
 
-    #results_bin = np.where(results >= 0, 1, -1).T
-
-    
-    #trainX_norm_nonzero_rqst, trainY_rqst = img_label_extractor(trainX_norm_nonzero, trainY, classifier)
-    # it should be with the entire trainY, not just the requested labeled trainY 
-
-    #weights = normal_eqn_solver(trainX_norm_nonzero_rqst, trainY)
-    
-    
-    # Now with weighted matrix 
-    # results = np.dot(norm_nonzero_img, weighted_matrix)
 
 
 if __name__ == "__main__":
